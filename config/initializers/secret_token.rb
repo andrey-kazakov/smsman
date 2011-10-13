81409bb6096868932a8b194846d70537bcb339eb
Lain Iwakura <lain@navi>
2011-10-13 15:32:30 +0400

    clean rails 3.1 app with mongoid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smsgate::Application.config.secret_token = '7d8821714f9751d93b34759da41e221f9110551888f3b1defd0e6084bdadfd02a75f59a5f07c8f1eaa9a6c087bca7b6670eb0ea03e4a43152ebe398d5baf1f67'
