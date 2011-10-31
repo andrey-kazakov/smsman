163d3d88ad6352d77a8ef2cc1b422f8e334c4adb
Timothy Klim <klimtimothy@gmail.com>
2011-10-31 12:47:12 +0300

    added for rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,7 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-
-require 'rvm/capistrano' # Для работы rvm
-#require 'bundler/capistrano'
 load 'deploy/assets'
 
 set :stages, %w(production)
 set :default_stage, "production"
-
-set :rvm_ruby_string, '1.9.3-rc1'
-set :rvm_type, :user
 
 set :application, "smsman"
 role :web, "smsman.myhotspot.ru"                          
@@ -23,14 +16,17 @@ set :scm_verbose, true
 set :use_sudo, false
 set :unicorn_script, "/etc/init.d/smsman"
 
-
-# require multistage. must be here! 
-#require 'capistrano/ext/multistage'
-
 default_run_options[:pty] = true
 ssh_options[:user] = "deploy"
 ssh_options[:forward_agent] = true
 
+# For rbenv
+set :default_environment, { 
+  'PATH' => "/home/deploy/.rvm/rubies/ruby-1.9.3-rc1/bin/:/home/deploy/.rvm/gems/ruby-1.9.3-rc1/bin:/home/deploy/.rvm/bin:$PATH",
+  'RUBY_VERSION' => 'ruby 1.9.3-rc1',
+  'GEM_HOME' => '/home/deploy/.rvm/gems/ruby-1.9.3-rc1/',
+  'GEM_PATH' => '/home/deploy/.rvm/gems/ruby-1.9.3-rc1/' 
+}
 
 namespace :deploy do  
   task :bundle do
@@ -41,13 +37,11 @@ namespace :deploy do
     run "cp #{shared_path}/config/* #{release_path}/config"
   end
 
- task :restart do
-  run "/etc/init.d/smsman restart"
- end
-
-
+  task :restart do
+    run "/etc/init.d/smsman restart"
+  end
 end
 
-  after 'deploy:finalize_update', 'deploy:copy_configs'
-  before "deploy:assets:precompile", "deploy:bundle"
-  after :deploy, "deploy:restart"
+after 'deploy:finalize_update', 'deploy:copy_configs'
+before "deploy:assets:precompile", "deploy:bundle"
+after :deploy, "deploy:restart"
