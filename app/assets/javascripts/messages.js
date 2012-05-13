7f0e60ba2a9f72bfd93d77d22443447d8c0279ba
lain <lain@lain-desktop.(none)>
2012-05-14 03:39:25 +0400

    recipients input parsing

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,19 +1,59 @@
 // tools for messages editor
 (function()
 {
-
   var tools  =
   {
-      phoneRegex: /(?:tel:)?\+\d+(?:\s*(?:\(\d{3}\)\s*)*\d{3}(?:-|\s)*\d{2}(?:-|\s)*\d{2})?/g
-    //
+    phoneRegex: /(?:tel:)?\+(7|38)\s*[\(\)]?\d{3,}[\)\(]?\s*\d{3}[-\s]*\d{2}[-\s]*\d{2}/g
+    , sanitizeNumber: function(text)
+      {
+        return '+' + text.replace(/[^\d]/g, '')
+      }
+    , decorateNumber: function(number)
+      {
+        var parts = number.match(/\+(\d+)(\d{3})(\d{3})(\d{2})(\d{2})$/)
+        return parts ? ('+' + 
+                  parts[1] + ' (' +
+                    parts[2] + ') ' + 
+                      parts[3] + '-' + 
+                        parts[4] + '-' +
+                          parts[5]) : number
+      }
   }
 
-  $('article.message > div.recipients > input').live('keyup mouseup', function(event)
+  $('article.message > div.recipients').live('click', function(event)
+  {
+    if (!event.target || event.target.tagName.toLowerCase() != 'input') $(this).find('input:last').focus();
+  })
+  $('article.message > div.recipients > input').live('keyup blur', function(event)
   {
     var input = $(this);
     var value = input.val();
-    var match = value.match(tools.phoneRegex);
+    var matches = value.match(tools.phoneRegex);
+    if (matches)
+    {
+      for (var i = 0; i < matches.length; i++)
+      {
+        var number = tools.sanitizeNumber(matches[i]);
 
-    //
+        var link = $('<a></a>');
+        link.attr('href', 'tel:' + number);
+        link.text(tools.decorateNumber(number));
+
+        link.addClass('bubble').addClass('pseudolink').addClass('black');
+        
+        link.insertBefore(input);
+      }
+
+      var lastMatch = matches[matches.length - 1];
+
+      value = value.substr(value.lastIndexOf(lastMatch) + lastMatch.length);
+    }
+
+    input.val(value);
+    if (!value.trim().length && input.hasClass('bubble'))
+    {
+      input.parent('div').trigger('click');
+      input.remove();
+    }
   })
 })()
