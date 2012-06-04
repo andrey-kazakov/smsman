d8c9bf6cd516e2165e9d5f25381c25d3d2bd06ec
kuroineko <nvgator@gmail.com>
2012-06-04 21:41:32 +0400

    contacts editor optimizations

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -445,6 +445,7 @@
       var input = $(this);
 
       var number = tools.sanitizeNumber(input.attr('name'))
+      var old_name = Contacts.findNameByNumber(number);
 
       var isContact = input.hasClass('contact');
 
@@ -452,7 +453,8 @@
 
       if (isContact)
       {
-        Contacts.delay('pushContact')(number, value);
+        if (!value || value != old_name)
+          Contacts.delay('pushContact')(number, value);
       }
       else
       {
@@ -462,12 +464,11 @@
         }
         else if (!tools.phoneRegex.test(value))
         {
-          setTimeout(function() { input.caret({ start: 0, end: value.length }).focus() }, 1);
+          delay(input.caret({ start: 0, end: value.length }).focus)();
         }
         else
         {
           var new_number = tools.sanitizeNumber(value);
-          var old_name = Contacts.findNameByNumber(number);
 
           if (new_number != number)
           {
