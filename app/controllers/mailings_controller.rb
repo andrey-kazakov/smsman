662e632c14360ae65f930d2660af7dc7f2e1b5ac
kuroineko <nvgator@gmail.com>
2012-06-07 04:35:16 +0400

    / redirects to welcome page unless signed in

--- app/controllers/mailings_controller.rb
+++ app/controllers/mailings_controller.rb
@@ -1,8 +1,10 @@
 # encoding: utf-8
 class MailingsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:new]
 
   def new
+    return redirect_to welcome_pages_path unless user_signed_in?
+
     @mailing = Mailing.new
     @mailing.messages.new(text: "Это - тестовое сообщение, и оно обязательно должно быть длиной больше семидесяти символов.", recipients: [79067473526, 389067473526, 79655285838])
   end
