c09ee2ac9cbf046d92841a819305b4a72b9606de
kuroineko <nvgator@gmail.com>
2012-06-08 15:03:43 +0400

    mailinngs summary helper

--- app/helpers/mailings_helper.rb
+++ app/helpers/mailings_helper.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 module MailingsHelper
   def message_attributes message
     ret = { 
@@ -11,5 +12,26 @@ module MailingsHelper
     end
 
     raw ret.map{ |k,v| %<#{k}="#{v}"> }.join ' '
+  end
+
+  def summary of
+    ret  = %<<span>#{t('messages.total')}:\u00a0</span> >
+
+    ret << %<<span class="countries">>
+
+    total_by_prefixes = of.summary.total_by_prefixes.reject{ |k,v| v < 1 }
+
+    total_by_prefixes.keys.each_with_index do |prefix, index|
+
+      ret << %<<span>#{t('messages.prefixes')[prefix]}\u00a0— >
+
+      ret << %<<span>#{total_by_prefixes[prefix]}</span>>
+      ret << (index < (total_by_prefixes.size - 1) ? ",\u00a0" : '')
+
+      ret << %<</span>>
+
+    end
+
+    raw ret
   end
 end
