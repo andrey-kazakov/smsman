91597db2588a67378585519142dfde8f49d2f152
kuroineko <nvgator@gmail.com>
2012-06-18 12:33:40 +0400

    recipients structure and summary calculation

--- app/models/recipients_list.rb
+++ app/models/recipients_list.rb
@@ -6,7 +6,13 @@ class RecipientsList
   belongs_to :user
   validates_presence_of :user # to allow recipients upload into new mailing
 
+  # [ { n: number, s: state, i: api_id } ]
+  field :list, type: Array, default: []
+
   field :summary, type: Summary
+  attr_protected :summary
+  after_initialize :calc_summary
+  after_validation :calc_summary
 
   def << recipient
     #
@@ -14,5 +20,16 @@ class RecipientsList
 
   def self.parse recipients
     #
+  end
+
+protected
+  def calc_summary
+    summary = Summary.new
+
+    list.each do |recipient|
+      summary.add(recipient[:n], recipient[:s])
+    end
+
+    write_attribute :summary, summary.serialize(summary)
   end
 end
