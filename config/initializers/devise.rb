c60d868b73545e559e8aebba4d429da100fd6642
kuroineko <nvgator@gmail.com>
2012-06-28 11:16:04 +0400

    many fixes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
+  config.mailer_sender = "info@smsman.org"
 
   # Configure the class responsible to send e-mails.
   config.mailer = "Devise::Mailer"
@@ -216,8 +216,17 @@ Devise.setup do |config|
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
   #
-  # config.warden do |manager|
+  config.warden do |manager|
   #   manager.intercept_401 = false
   #   manager.default_strategies(:scope => :user).unshift :some_external_strategy
-  # end
+    manager.failure_app = Class.new(Devise::FailureApp) do
+      def redirect_url
+        welcome_pages_path
+      end
+
+      def respond
+        http_auth? ? http_auth : redirect
+      end
+    end
+  end
 end
